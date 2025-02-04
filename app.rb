8b6eeae90a982820aa1fd14f5896583d38f44642
Elliot Kvarnström <143512738+elliotkvarn@users.noreply.github.com>
2025-02-04 10:18:11 +0100

    v6
    börjat med slim filer men fungerar inte att visa innehållet

--- app.rb
+++ app.rb
@@ -2,4 +2,27 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
-require 'becrypt'
+require 'bcrypt'
+
+get('/') do
+  db = SQLite3::Database.new("db/database.db")
+  @entries = db.execute("SELECT name FROM entries")
+  slim(:home)
+end
+
+get('/add') do
+  slim(:addentry)
+end
+
+post('/create') do
+
+  db.execute("INTERT INTO entries(img) VALUES ?", [:filename])
+  #skapa sträng
+  path = File.join("/public/uploaded_pictures/",params[:file][:filename])
+
+  #spara bilden
+  File.write(path,File.read(params[:file][:tempfile]))
+
+  redirect("/")
+
+end
