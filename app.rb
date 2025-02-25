6ac2fd4e0297e4386e67ed4a7ff4bd5644abea33
Elliot Kvarnström <143512738+elliotkvarn@users.noreply.github.com>
2025-02-25 09:58:54 +0100

    vecka 9
    kan lägga till entries

--- app.rb
+++ app.rb
@@ -4,28 +4,39 @@ require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
 
+enable :sessions
+
 get('/') do
   db = SQLite3::Database.new("db/database.db")
-  @entries = db.execute("SELECT name FROM entries")
-  slim(:home)
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM entries")
+  slim(:"home",locals:{home:result})
 end
 
 get('/add') do
-  p "hej"
   slim(:addentry)
 end
 
 post('/create') do
-  if params[:file] && params[:file][:filename]
-    filename = params[:file][:filename]
-    file = params[:file][:tempfile]
-  db.execute("INSERT INTO entries(img) VALUES ?", [:filename])
-  #skapa sträng
-  path = File.join("/public/uploaded_pictures/",params[:file][:filename])
+  entryname = params[:entryname]
+  creator = params[:creator]
+  category = params[:category]
+  date = params[:releaseyear]
+  img = params[:file]
+  description = params[:description]
+  tags = params[:tags]
+  date2 = params[:date2]
 
-  #spara bilden
-  File.write(path,File.read(params[:file][:tempfile]))
-
+  db = SQLite3::Database.new("db/database.db")
+  db.results_as_hash = true
+  if img && img[:filename]
+      filename = img[:filename]
+      file = img[:tempfile]
+      path = "./public/img/#{filename}"
+      File.open(path, 'wb') do |f|
+          f.write(file.read)  
+      end
+  end
+  result = db.execute("INSERT INTO entries (name,creator,category,description,tags,date,date2,img) VALUES(?,?,?,?,?,?,?,?)",[entryname,creator,category,description,tags,date,date2,filename])
   redirect("/")
-
 end
