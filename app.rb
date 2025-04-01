38d54ae97c2528037426e6177e5c877a79fe1846
Elliot Kvarnström <143512738+elliotkvarn@users.noreply.github.com>
2025-04-01 08:40:29 +0200

    v ?

--- app.rb
+++ app.rb
@@ -40,3 +40,45 @@ post('/create') do
   result = db.execute("INSERT INTO entries (name,creator,category,description,tags,date,date2,img) VALUES(?,?,?,?,?,?,?,?)",[entryname,creator,category,description,tags,date,date2,filename])
   redirect("/")
 end
+
+post('/register') do
+  username = params[:username]
+  password = params[:password]
+  password_confirm = params[:password_confirm]
+
+  db = SQLite3::Database.new('db/database.db')
+  db.results_as_hash = true
+  result = db.execute('SELECT id FROM users WHERE username=?',username)
+
+  if result.empty?
+    if (password == password_confirm)
+      password_digest = BCrypt::Password.create(password)
+      db.execute("INSERT INTO users (username,pw) VALUES (?,?)", [username, password_digest])
+      redirect('/')
+    else
+      set_error("Password don't match")
+      redirect('/register')
+    end
+  else
+    set_error('Username already exists')
+    redirect('/register')
+end
+
+post('/login') do
+  username = params["username"]
+  password = params["password"]
+  
+  db = SQLite3::Database.new('db/db.db')
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+  pwdigest = result["pwdigest"]
+  id = result["id"]
+
+
+  if BCrypt::Password.new(pwdigest) == password
+    session[:id] = id
+    redirect('/todos')
+  else
+    "fel lösen!!"
+  end
+end
